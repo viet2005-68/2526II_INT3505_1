fcbb821c3954bc357b3ebd878ff3e380a01710cc
viet2005-68 <23020170@vnu.edu.vn>
2026-05-07 18:57:37 +0700

    add deprecation warnings for v1 endpoint

--- week9/router/v1.py
+++ week9/router/v1.py
@@ -8,7 +8,6 @@ def create():
     data = request.get_json()
     return create_payment(data)
 
-
 @v1_bp.route('/', methods=['GET'])
 def get_all():
     return get_all_payments()
@@ -25,3 +24,11 @@ def update_payment_route(id):
 @v1_bp.route('/<id>', methods=['DELETE'])
 def delete_payment_route(id):
     return delete_payment(id)
+
+@v1_bp.after_request
+def add_deprecation_headers(response):
+    response.headers['Warning'] = '299 - This API v1 is deprecated. Please migrate to v2.'
+    response.headers['Deprecation'] = 'true'
+    response.headers['Sunset'] = 'Wed, 31 Dec 2025 23:59:59 GMT'
+    response.headers['Link'] = '</api/v2/payments>; rel="successor-version"'
+    return response
