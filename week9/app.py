5af6dd78772d7b404b91ed35ebbdc955d4bdaa6f
viet2005-68 <23020170@vnu.edu.vn>
2026-05-07 18:59:35 +0700

    blueprint in mainmain

--- week9/app.py
+++ week9/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from db import init_db
-from router import v1
+from router import v1, v2, payment
 
 app = Flask(__name__)
 
@@ -8,6 +8,8 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/payment_db"
 
 init_db(app)
 app.register_blueprint(v1.v1_bp, url_prefix='/api/v1/payments')
+app.register_blueprint(v2.bp, url_prefix='/api/v2/payments')
+app.register_blueprint(payment.bp, url_prefix='/api/payments')
 
 @app.route('/')
 def index():
